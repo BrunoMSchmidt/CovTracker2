23dab6b15c248ae09391ba3fbf1a5f15caaec9df
Bruno M Schmidt <brunomschmidt1@gmail.com>
2021-11-16 20:49:13 -0300

    Adicionado projeto Spring Boot Adicionada tabela para autenticacao

---
+++ API/covtracker/src/test/java/com/covtracker/covtracker/CovtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covtracker.covtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
