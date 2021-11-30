b1d107c8209adb1f67aa871464b7b83b481c4e1a
Marciel de lara <marcielviniciusdelara@gmail.com>
2021-11-29 22:03:43 -0300

    Iniciado script para permissoes de acesso

---
+++ Banco/Scripts/permissoes-acesso.sql
@@ -0,0 +1,24 @@
+create group usuario;
+create group funcionario;
+create group gerente;
+
+
+--  Permissões para todas as tabelas do banco para o administrador do sistema--
+grant select, insert, update, delete
+on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, tipo_usuario, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
+to gerente;
+
+--Permissões de acesso para funcionarios do sistema--
+grant select, update, insert
+on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
+to funcionario;
+
+--Permissões de acesso para usuarios do sitema--
+grant select
+on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
+to usuario;
+
+grant gerente to administrador;
+
+
+
