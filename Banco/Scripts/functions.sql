2f126fe21b1fc249396b057a506e2a114f7f6002
Marciel de lara <marcielviniciusdelara@gmail.com>
2021-12-01 21:19:02 -0300

    dado sequencia nas funções de banco

--- Banco/Scripts/functions.sql
+++ Banco/Scripts/functions.sql
@@ -13,7 +13,32 @@ end
 $body$
 language plpgsql;
 
-select conta_usuarios();
+select conta_usuarios(); 
+
+--Criando função para contagem de registro 
+create or replace function conta_registro(nome_tabela varchar)
+returns setof record
+as
+$body$
+declare
+	tabela varchar; 
+	conta numeric;
+	rec record;
+begin 
+	--pg-tables
+	for tabela in (select tablename from pg_tables
+	where tablename ilike nome_tabela and schemaname = 'public')
+	loop
+	---select count(*) from tabela; da erro
+	execute format('select count(*) from %I', tabela) into conta;
+	return next rec; --retorna o record
+	end loop;
+end
+$body$
+language plpgsql;
+
+select * from conta_registro('comorbidade c %')
+as (tabela varchar, conta numeric);
 
 --CRIAR TRIGGER PARA VERIFICAÇÃO DA VACINA
 -- PARA NÃO PODER TOMAR A VACINA ANTES OU EM ATRASO DO PRAZO
