da1b5ebee3991f662c91cb116083b2e160532467
Marciel de lara <marcielviniciusdelara@gmail.com>
2021-12-06 19:35:23 -0300

    ajustado permissões de acesso

--- Banco/Scripts/permissoes-acesso.sql
+++ Banco/Scripts/permissoes-acesso.sql
@@ -1,22 +1,22 @@
-create group usuario;
+create group paciente;
 create group funcionario;
-create group gerente;
+create group administrador;
 
 
 --  Permissões para todas as tabelas do banco para o administrador do sistema--
 grant select, insert, update, delete
 on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, tipo_usuario, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
-to gerente;
+to administrador;
 
 --Permissões de acesso para funcionarios do sistema--
 grant select, update, insert
 on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
 to funcionario;
 
---Permissões de acesso para usuarios do sitema--
-grant select
+--Permissões de acesso para paciente do sitema--
+grant select, update
 on cidade, comorbidade, empresa, endereco, estado, orientacao, sintoma, teste, usuario, usuario_comorbidade, usuario_sintoma, usuario_telefone, vacina, usuario_vacina
-to usuario;
+to paciente;
 
 
 
