59e068f7573a4d892ec31b47bd09a994f96c6ec5
Bruno M Schmidt <brunomschmidt1@gmail.com>
2021-12-06 21:20:31 -0300

    Adicionado redirect para a pagina de detalhes do usuario, partindo da pagina de usuarios, e adicionado javadoc

--- src/main/java/com/covtracker/covtracker/controllers/UsuarioController.java
+++ src/main/java/com/covtracker/covtracker/controllers/UsuarioController.java
@@ -10,21 +10,35 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Este Controller Rest é responsável pelos endpoints relacionadas ao Usuário
+ * @Author Bruno Schmidt
+ */
 @RestController()
 @RequestMapping(path = "/api")
 public class UsuarioController {
 
+    /**
+     * Repositorio do Usuário.
+     */
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @Autowired
-    CustomUserDetailsService usersDetailService;
-
+    /**
+     * Lista todos os usuários
+     * @return Lista lista de usuários
+     */
     @GetMapping(path = "/usuarios")
     public List<Usuario> listAll() {
         return this.usuarioRepository.findAll();
     }
 
+    /**
+     * Encontra o usuário pelo cpf.
+     *
+     * @param cpf CPF do usuário
+     * @return usuario
+     */
     @GetMapping(path = "/usuario/{cpf}")
     public ResponseEntity<Usuario> getByCpf(@PathVariable() BigDecimal cpf) {
         return this.usuarioRepository.findById(cpf)
@@ -32,16 +46,35 @@ public class UsuarioController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * Encontra usuários contendo determinada string.
+     *
+     * @param name a string
+     * @return Lista dos usuários
+     */
     @GetMapping(path = "/usuarios/findByName/{name}")
-    public List<Usuario> getByCpf(@PathVariable() String name) {
+    public List<Usuario> getByNome(@PathVariable() String name) {
         return this.usuarioRepository.findByNomeContainingIgnoreCase(name);
     }
 
+    /**
+     * Salva usuário.
+     *
+     * @param usuario O usuário
+     * @return O usuário
+     */
     @PostMapping(path = "/usuario")
     public Usuario create(@RequestBody Usuario usuario){
         return usuarioRepository.save(usuario);
     }
 
+    /**
+     * Atualiza usuário.
+     *
+     * @param cpf     O cpf
+     * @param usuario O usuário
+     * @return O usuário atualizado
+     */
     @PutMapping(path = "/usuario/{cpf}")
     public ResponseEntity<Usuario> update(@PathVariable() BigDecimal cpf, @RequestBody Usuario usuario){
         return usuarioRepository.findById(cpf)
@@ -60,6 +93,12 @@ public class UsuarioController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * Deleta o usuário
+     *
+     * @param cpf O cpf
+     * @return ResponseEntity vazia
+     */
     @DeleteMapping(path = "/usuario/{cpf}")
     public ResponseEntity delete(@PathVariable() BigDecimal cpf){
         return usuarioRepository.findById(cpf)
